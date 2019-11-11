2cf004016b9c1b2b10cdebff1e1a5cc5637869d1
Sweatha Edpuganti <swedpuga@in.ibm.com>
2019-11-11 16:27:09 +0530

    updating

--- final-framework-testng/assignments/Simple/TC001.java
+++ final-framework-testng/assignments/Simple/TC001.java
@@ -1,5 +1,22 @@
 package Simple;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class TC001 {
 
+	@Test
+	public void login() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://magento.com");
+		driver.findElement(By.className("fa-user")).click();
+		driver.quit();
+		
+	}
 }
